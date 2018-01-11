ee494912c9e43c574904b079791d0e74486acf36
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2018-01-11 23:36:28 +0100

    Reformat

--- spring-amqp-listener/app.groovy
+++ spring-amqp-listener/app.groovy
@@ -1,11 +1,11 @@
 @EnableRabbit
 @Component
 class MyListener {
-	@RabbitListener(bindings = @QueueBinding(value = @Queue,
-											 exchange = @Exchange(value = "\${exchange.name}", 
-											 	                  type = "\${exchange.type}"),
-											 key = "\${routing.key}"))
-	def listen(message) {
-		println "Received message ${new String(message.body)}"
-	}
+    @RabbitListener(bindings = @QueueBinding(value = @Queue,
+                    exchange = @Exchange(value = "\${exchange.name}", 
+                    type = "\${exchange.type}"),
+                    key = "\${routing.key}"))
+    def listen(message) {
+        println "Received message ${new String(message.body)}"
+    }
 }
